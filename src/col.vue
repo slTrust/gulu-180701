0eb50d954008016571266fbb49e1a91744904d3f
huangjx <trustfor@sina.cn>
2018-07-11 20:27:59 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+    >
+        <div style="border:1px solid green;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -8,7 +12,17 @@
     export default {
         name:'GuluCol',
         props:{
-            span:[Number,String]
+            span:{
+                type:[Number,String]
+            },
+            offset:{
+                type:[Number,String]
+            }
+        },
+        data(){
+            return {
+                gutter:0
+            }
         }
     }
 </script>
@@ -19,14 +33,20 @@
         height: 100px;
         background: #ccc;
         width:50%;
-
         $class-prefix: col-;
-
         @for $n from 1 through 24 {
 
             // for each $col_#{i}
             &.#{$class-prefix}#{$n} {
                 width:($n/24)*100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+
+            // for each $col_#{i}
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,26 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        name:"GuluRow",
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        created(){
+        },
+        mounted(){
+            this.$children.forEach((vm)=>{
+                vm.gutter = this.gutter
+            })
+        }
+
+    }
 </script>
 
 <style scoped lang="scss">
